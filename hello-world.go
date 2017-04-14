f70583a10da73fcd9fe616678788827a6bd0d525
Mike Deiters <miked49er@gmail.com>
2017-04-14 11:13:49 -0400

    Obligitory Hello World using http

---
+++ hello-world.go
@@ -0,0 +1,15 @@
+// hello-world.go
+package main
+
+import (
+  "fmt"
+  "net/http"
+)
+
+func main() {
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Hello World")
+  })
+
+  http.ListenAndServe(":8080", nil)
+}
